88b1ac3f1d640f1ecad5222bd7768dfc61a33972
hadeelelmadhoon <44147485+hadeelelmadhoon@users.noreply.github.com>
2019-09-14 02:58:32 -0400

    Create imageClassifier.py

---
+++ imageClassifier.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+url = "https://canadacentral.api.cognitive.microsoft.com/vision/v1.0/analyze"
+
+querystring = {"visualFeatures":"Tags","language":"en"}
+
+headers = {
+    'Ocp-Apim-Subscription-Key': "dc97ef487b364479b29d5f98e017c28e",
+    'Content-Type': "application/octet-stream"
+    }
+data = open('/Users/hadeelelmadhoon/Documents/STOP_sign.jpg', 'rb').read()
+response = requests.request("POST", url, headers=headers, data=data, params=querystring)
+
+result = json.loads(response.text)
+
+print(result['tags'][0])
